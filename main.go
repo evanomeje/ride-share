d1fc9a089d0063eb7500f89db686181f00d99f7a
evan.omeje <evan@gmail.com>
2025-01-05 19:42:11 +0000

    starting application

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func getData(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello world\n")
+}
+
+func main() {
+	http.HandleFunc("/data", getData)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	serverEnv := os.Getenv("SERVER_ENV")
+
+	if serverEnv == "DEV" {
+		http.ListenAndServe(":8080", nil)
+	} else if serverEnv == "PROD" {
+		http.ListenAndServeTLS(
+			":443",
+			"/etc/letsencrypt/live/app.evanomeje.xyz/fullchain.pem",
+			"/etc/letsencrypt/live/app.evanomeje.xyz/privkey.pem",
+			nil,
+		)
+	}
+}
