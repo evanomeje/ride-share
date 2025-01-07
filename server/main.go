b9a7ab0844c25183bbc9d39a1e8773eb4ed02c9c
chima <evanomeje@gmail.com>
2025-01-07 12:33:00 -0700

    added server genrated data for rides

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"encoding/json"
 )
 
 func getDrivers(w http.ResponseWriter, req *http.Request) {
@@ -33,12 +34,42 @@ func getDrivers(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, data)
 }
 
+type Ride struct {
+	Id       string `json:"id"`
+	CarId    string `json:"car_id"`
+	Location string `json:"location"`
+	Path     string `json:"path"`
+}
+
+func getRides(w http.ResponseWriter, req *http.Request) {
+	rows, err := db.Connection.Query("SELECT * FROM rides")
+	if err != nil {
+		http.Error(w, "Failed to get rides: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var rides []Ride
+
+	for rows.Next() {
+		var ride Ride
+		rows.Scan(&ride.Id, &ride.CarId, &ride.Location, &ride.Path)
+		rides = append(rides, ride)
+	}
+
+	ridesBytes, _ := json.MarshalIndent(rides, "", "\t")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(ridesBytes)
+}
+
 func main() {
 	db.InitDB()
 	defer db.Connection.Close()
 
 	http.Handle("/", http.FileServer(http.Dir("../frontend/build")))
 	http.HandleFunc("/drivers", getDrivers)
+	http.HandleFunc("/rides", getRides)
 
 	serverEnv := os.Getenv("SERVER_ENV")
 
